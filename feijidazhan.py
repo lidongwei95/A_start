a9f77a22ef2ca3d3ff9791bd8663a1f84c799072
dongweili <ldwfive@163.com>
2022-09-10 10:26:15 +0800

    update move

--- feijidazhan.py
+++ feijidazhan.py
@@ -1,7 +1,6 @@
-from turtle import back
 import pygame
 from pygame import *
-
+import time
 
 image_path = "/home/dongweili/Pictures/"
 
@@ -13,13 +12,19 @@ def main():
     background = pygame.image.load(image_path + "beijing.png")
     # 创建一个图片，玩家的飞机
     player = pygame.image.load(image_path + "wo.png")
-    # 3. 将背景图片贴到窗口中
-    screen.blit(background, (0,0))
-    # 将主飞机图片贴到窗口中
-    screen.blit(player, (480/2 - 100/2, 600))
+
+    x = 480 / 2 - 100 / 2
+    y = 600
+
+    # 飞机速度
+    speed = 10
 
     # 让屏幕一直显示在窗口
     while True:
+        # 3. 将背景图片贴到窗口中
+        screen.blit(background, (0,0))
+        # 将主飞机图片贴到窗口中
+        screen.blit(player, (x, y))
         # 获取事件
         for event in pygame.event.get():
             # 判断事件类型
@@ -28,19 +33,26 @@ def main():
                 pygame.quit()
                 # python程序退出
                 exit()
-            # 响应键盘事件
-            elif event.type == pygame.KEYDOWN:
-                # 检验按键是否是a或者left
-                if event.key == K_a or event.key == pygame.K_LEFT:
-                    print("left")
-                elif event.key == K_d or event.key == pygame.K_RIGHT:
-                    print("right")
-                elif event.key == pygame.K_w or event.key == pygame.K_UP:
-                    print("up")
-                elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                    print("down")
+        # 监听键盘事件
+        key_pressed = pygame.key.get_pressed()
+        if key_pressed[K_w] or key_pressed[K_UP]:
+            print("up")
+            y -= speed
+        if key_pressed[K_s] or key_pressed[K_DOWN]:
+            print("down")
+            y += speed
+        if key_pressed[K_a] or key_pressed[K_LEFT]:
+            print("left")
+            x -= speed
+        if key_pressed[K_d] or key_pressed[K_RIGHT]:
+            print("right")
+            x += speed
+        if key_pressed[K_SPACE] :
+            print("space")
+
         # 4. 显示窗口中的内容
         pygame.display.update()
+        time.sleep(0.01)
 
 
 
